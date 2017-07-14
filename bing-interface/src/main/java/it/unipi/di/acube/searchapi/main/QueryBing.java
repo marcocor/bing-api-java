bb129357d5901e646ad3aa465bcd99baed529e89
marco <marco@macor.di.unipi.it>
2017-07-14 15:30:25 +0200

    Fixed Javadoc.

--- bing-interface/src/main/java/it/unipi/di/acube/searchapi/main/QueryBing.java
+++ bing-interface/src/main/java/it/unipi/di/acube/searchapi/main/QueryBing.java
@@ -7,6 +7,18 @@ import it.unipi.di.acube.searchapi.model.WebsearchResponseEntry;
 
 public class QueryBing {
 
+    /**
+     * This java main issues a query to the Bing API and prints results.
+     * 
+     * @param args
+     *            The command-line arguments in the following order: BING_KEY, QUERY, N_RESULTS, CACHE_FILE. BING_KEY is the
+     *            authentication key to the Bing Search API; QUERY is the query to issue; N_RESULTS is the number of results
+     *            needed (if this exceeds the number of results returned by one call, multiple call will be issued), CACHE_FILE is
+     *            the name of the file where results are stored.
+     * 
+     * @throws Exception
+     *             if something went wrong.
+     */
     public static void main(String[] args) throws Exception {
         BingSearchApiCaller bing = new BingSearchApiCaller(args[0]);
         CachedWebsearchApi cached = CachedWebsearchApi.builder().api(bing).path(args[3]).create();
--- bing-interface/src/main/java/it/unipi/di/acube/searchapi/main/QueryGoogle.java
+++ bing-interface/src/main/java/it/unipi/di/acube/searchapi/main/QueryGoogle.java
@@ -7,6 +7,19 @@ import it.unipi.di.acube.searchapi.model.WebsearchResponseEntry;
 
 public class QueryGoogle {
 
+    /**
+     * This java main issues a query to the Google CSE API and prints results.
+     * 
+     * @param args
+     *            The command-line arguments in the following order: GOOGLE_CSE_ID, GOOGLE_API_KEY, CACHE_FILE, QUERY, N_RESULTS.
+     *            GOOGLE_CSE_ID is the ID of the custom search engine you want to query, GOOGLE_API_KEY is the authentication key
+     *            released by Google, CACHE_FILE is the name of the file where results are stored, QUERY is the query to issue,
+     *            N_RESULTS is the number of results needed (if this exceeds the number of results returned by one call, multiple
+     *            call will be issued).
+     * 
+     * @throws Exception
+     *             if something went wrong.
+     */
     public static void main(String[] args) throws Exception {
         GoogleSearchApiCaller google = new GoogleSearchApiCaller(args[0], args[1]);
         CachedWebsearchApi cached = CachedWebsearchApi.builder().api(google).path(args[2]).create();
